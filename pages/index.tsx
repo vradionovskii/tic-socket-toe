bc7b8a95b364e1dbe382ccfc2ce3609d98cfb5c9
vradionovskii <vradionovskii@yandex.ru>
2021-08-22 11:51:23 +0300

    worjing prototype

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,74 @@
 import { useRouter } from "next/router";
 import Layout from "../components/Layout";
+import { io } from "socket.io-client";
+import { useEffect, useRef, useState } from "react";
 
 const IndexPage: React.FC = () => {
+  const symbol = useRef(null);
+  const initialState = [null, null, null, null, null, null, null, null, null];
+  const [state, setstate] = useState([...initialState]);
+
+  const socket = io("http://localhost:3001", { transports: ["websocket"] });
+
+  socket.on("updateState", (message) => {
+    if (state !== message) {
+      setstate([...message]);
+    }
+  });
+
+  useEffect(() => {
+    if (window.location.hash === "" && state === initialState) {
+      socket.on("message", (message) => {
+        window.location.hash = message + "&x";
+        socket.emit("joinRoom", {
+          id: message,
+          data: state,
+        });
+      });
+    } else {
+      socket.emit("joinRoom", {
+        id: window.location.hash.slice(1).split("&")[0],
+        data: initialState,
+      });
+      if (window.location.hash.indexOf("&") === -1) {
+        window.location.hash += "&o";
+      }
+    }
+    symbol.current = window.location.hash.slice(1).split("&")[1];
+  }, []);
+
+  function updateBoard(i) {
+    const temp = state;
+    temp[i] = symbol.current === "x" ? true : false;
+    setstate([...temp]);
+    socket.emit("updateState", {
+      id: window.location.hash.slice(1).split("&")[0],
+      data: state,
+    });
+  }
+
   const router = useRouter();
+
   return (
-    <Layout currentPage={router.route} title="Home">
-      <h1>Welcome!</h1>
+    <Layout currentPage={router.route} title="home">
+      <div
+        className={`${
+          state.filter((i) => i === symbol.current).length % 2 === 0
+            ? "pointer-events-all"
+            : "pointer-events-"
+        } grid max-w-sm grid-cols-3 gap-8 mx-auto`}
+      >
+        {state.map((mark, i) => (
+          <button
+            className={`${mark ? "pointer-events-none" : ""} p-4 bg-gray-400`}
+            onClick={() => updateBoard(i)}
+            key={i}
+          >
+            {mark && "x"}
+            {mark === false && "o"}
+          </button>
+        ))}
+      </div>
     </Layout>
   );
 };
---
+++ server.mjs
@@ -0,0 +1,41 @@
+import { Server } from "socket.io";
+
+const rooms = [];
+const io = new Server(3001, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  io.emit("message", socket.id);
+
+  socket.on("joinRoom", (msg) => {
+    socket.join(msg.id);
+    io.sockets.in(msg.id).emit("sendID", msg.id);
+
+    if (rooms.findIndex((i) => i.id === msg.id) !== -1) {
+      io.sockets
+        .in(msg.id)
+        .emit(
+          "updateState",
+          rooms[rooms.findIndex((i) => i.id === msg.id)].data
+        );
+    } else {
+      rooms.push({
+        id: msg.id,
+        data: msg.data,
+      });
+    }
+    console.log(rooms);
+    console.log("----------------");
+  });
+
+  socket.on("updateState", (message) => {
+    rooms[rooms.findIndex((i) => i.id === message.id)].data = message.data;
+    console.log(rooms);
+    console.log("_______________");
+    io.sockets.in(message[0]).emit("updateState", message.data);
+  });
+});
